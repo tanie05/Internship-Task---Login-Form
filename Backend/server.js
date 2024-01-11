7df76389b466cd3d6118516543db3fb53a6602bb
Tanmay Sangtani <tanmaysangtani04@gmail.com>
2024-01-11 13:17:07 +0530

    Implemented login API and added password hashing

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,47 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const md5 = require('md5') 
 const app = express();
 const port = 3000; // You can change the port number if needed
+
+//CORS
+var cors = require('cors')
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+};
+app.use(cors(corsOptions))
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    console.log(`Received username: ${username}, password: ${password}`);
-    res.send('Login successful');
+    let hashedpw = ''
+    hashedpw = md5(password)
+    console.log(hashedpw)
+    console.log(`Received username: ${username}, password: ${password}, hashedpw: ${hashedpw}`);
+
+    
+    //hashing the password. 
+    //Step 1 - check whether password length > 0.
+    //Step 2 - hash the password
+    if (password.length > 0) {
+      hashedpw = md5(password)
+      console.log(hashedpw)
+    }
+    else {
+      console.log("Minimum length error")
+    }
+
   });
 
 app.listen(port, () => {
--- Frontend/script.js
+++ Frontend/script.js
@@ -4,12 +4,33 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+
+
     
+    // Make a POST request to the Node.js server
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        //On successful of login
+        console.log(data)
+      })
+      .catch(error => {
+        console.error('Error sending data:', error);
+        document.getElementById('error-message').textContent = 'Some error occurred';
+      });
     
-    if (username === 'intern' && password === 'training123') {
-      window.location.href = 'http://localhost:3000/'; 
-    } else {
-      document.getElementById('error-message').textContent = 'Invalid username or password';
-    }
+    // if (username === 'intern' && password === 'training123') {
+    //   window.location.href = 'http://localhost:3000/'; 
+    // } else {
+    //   document.getElementById('error-message').textContent = 'Invalid username or password';
+    // }
   });
+
   
+
