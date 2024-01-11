8c840ba20f8096a295232b81b1c3640c641466b1
Tanvi Sharma <tanvisharma@Tanvis-MacBook-Pro.local>
2024-01-11 11:30:53 +0530

    removed node_modules folder

--- Frontend/script.js
+++ Frontend/script.js
@@ -5,8 +5,8 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
 
+    
     if(ValidateEmail(email)){
-      // VerifyEmail(email)
       window.location.href = 'http://localhost:3000/'; 
     }else{
       document.getElementById('error-message').textContent = 'Invalid email';
@@ -15,45 +15,16 @@ document.getElementById('login-form').addEventListener('submit', function(event)
   });
   
 
-
   function ValidateEmail(input) {
 
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    
-    if (input.match(validRegex)) {
+    var validRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
+    if(input.match(validRegex) !== null){
       return true;
-  
-    } else {
-  
-      alert("Invalid email address!");
-  
+    }else{
+      alert("Invalid email address");
       return false;
-  
     }
-  
+
   }
-  // import axios from 'axios';
 
-  // async function VerifyEmail(input){
-
-  //   const options = {
-  //     method: 'GET',
-  //     url: 'https://quickemailverification.p.rapidapi.com/v1/verify',
-  //     params: {
-  //       email: input
-  //     },
-  //     headers: {
-  //       Authorization: '<REQUIRED>',
-  //       'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-  //       'X-RapidAPI-Host': 'quickemailverification.p.rapidapi.com'
-  //     }
-  //   };
-    
-  //   try {
-  //     const response = await axios.request(options);
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
