b5e61f6a192fb484c3752c248b9c744015c21384
Abhinav Mishra <monsterofcrania@gmail.com>
2024-01-11 16:17:51 +0530

    task done

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,60 @@
 const express = require('express');
+const mysql = require('mysql')
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000; // You can change the port number if needed
+
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    console.log(`Received username: ${username}, password: ${password}`);
-    res.send('Login successful');
-  });
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'login_form',
+})
+
+db.connect(function(err) {
+  if (err) console.log(err);
+  console.log("Connected!");
+  // let sql = "INSERT INTO customers (name, ÷address) VALUES ('Company Inc', 'Highway 37')";
+});
+
+app.post('/login', (req, res)=>{
+  
+  const {email, password} = req.body;
+  console.log(email, password);
+  const sql ='SELECT login_count from user2 where email = ? and password = ?';
+  db.query(sql, [email, password], (err , result)=>{
+    if(err) throw err;
+    console.log(result);
+    if(!result[0].login_count){
+      res.status(200).send({success: true, change: true});
+    }
+    else {
+      const sql = `UPDATE user2 SET login_count = ? where email = ?`;
+      db.query(sql, [result[0].login_count+1, email],(err, result)=>{
+        if(err) throw err;
+        res.status(200).send({success: true, change: false});
+      })
+      
+    }
+  })
+})
+
+// app.post('/login', (req, res) => {
+//     const { username, password } = req.body;
+//     console.log(`Received username: ${username}, password: ${password}`);
+//     res.send('Login successful');
+//   });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
--- Frontend/script.js
+++ Frontend/script.js
@@ -1,15 +1,37 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+  event.preventDefault();
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  // Make a POST request to the Node.js server
+  fetch('http://10.181.22.152:3000/login', {
+    method: 'POST',
     
-    
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    
-    
-    if (username === 'intern' && password === 'training123') {
-      window.location.href = 'http://localhost:3000/'; 
-    } else {
-      document.getElementById('error-message').textContent = 'Invalid username or password';
-    }
-  });
+    headers: {
+      'Content-Type': 'application/json',
+      
+    },
+    body: JSON.stringify({ email: username, password: password }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      //On successful of login
+      if(data.change){
+        alert('Change your password');
+      }
+      else {
+        alert('Logged In');
+      }
+    })
+    .catch(error => {
+      console.error('Error sending data:', error);
+      document.getElementById('error-message').textContent = 'Some error occurred';
+    });
+  if (username === 'intern' && password === 'training123') {
+    window.location.href = 'http://localhost:3000/';
+  } else {
+    document.getElementById('error-message').textContent = 'Invalid username or password';
+  }
+});
+
+
   
